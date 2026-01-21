a992e50fbfa39be74fcf61867c3bac7e4766bb8d
Koda Turqui <twkod@pm.me>
2026-01-21 09:17:43 -0300

    first commit
    project base files

---
+++ src/main/java/com/edu/marble_and_arch/MarbleAndArchApplication.java
@@ -0,0 +1,13 @@
+package com.edu.marble_and_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarbleAndArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarbleAndArchApplication.class, args);
+	}
+
+}
