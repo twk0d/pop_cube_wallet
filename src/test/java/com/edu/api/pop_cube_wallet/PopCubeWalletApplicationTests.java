4a266b66b7f5e24b074f4475b8b640bdc2e8bedf
twk0d <twkod@pm.me>
2026-02-16 10:41:29 -0300

    test: configure Testcontainers for PostgreSQL

--- src/test/java/com/edu/api/pop_cube_wallet/PopCubeWalletApplicationTests.java
+++ src/test/java/com/edu/api/pop_cube_wallet/PopCubeWalletApplicationTests.java
@@ -2,8 +2,10 @@ package com.edu.api.pop_cube_wallet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PopCubeWalletApplicationTests {
 
   @Test
